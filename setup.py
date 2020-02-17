c994c930aad1f81ae5545902dc6474311384e2cd
Yoram Michaeli <yoram.michaeli@radcom.com>
2020-02-17 15:52:35 +0200

    [master] Add Jenkinsfile {2020-02-17 13:52}

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: Apache License",
+         "License :: OSI Approved :: Apache Software License",
          "Operating System :: OS Independent",
      ],
  )
